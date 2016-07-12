1171824bc75a1d42c35c9b1bbf095650e847c661
Dario Martins Silva <dario@live.ca>
2016-07-12 15:25:53 -0400

    remove css require: JSUI-933 (#81)
    * remove css require
    https://coveord.atlassian.net/browse/JSUI-933

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ if (minimize) {
 
 // Fail plugin will allow the webpack ts-loader to fail correctly when the TS compilation fails
 var plugins = [failPlugin];
+
 if (minimize) {
   plugins.push(new webpack.optimize.UglifyJsPlugin());
 }
@@ -41,9 +42,7 @@ module.exports = {
   devtool: 'source-map',
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts-loader' },
-      { test: /\.scss$/, loaders: ['style?insertAt=bottom', 'css?sourceMap', 'resolve-url', 'sass?sourceMap'] },
-      { test: /\.(gif|svg|png|jpe?g|ttf|woff2?|eot)$/, loader: 'url?limit=8182' }
+      { test: /\.ts$/, loader: 'ts-loader' }
     ]
   },
   plugins: plugins,
